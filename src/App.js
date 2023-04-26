e67bb994e5273e9336434848d87b53181671b713
Vu Trong Dat <datvuhp2002@gmail.com>
2023-04-26 14:47:08 +0700

    Cau hinh lai route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./Pages/Routes";
-import Home from "./Pages/Home";
 function App() {
   return (
-    <Router>
+    <Router basename="/MovieWeb">
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
